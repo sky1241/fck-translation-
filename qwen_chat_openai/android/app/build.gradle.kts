a45b3ac98d4baf3fba05535be4911eafedc1b4c4
sky1241 <ludovik1241@gmail.com>
2025-10-05 22:15:58 +0200

    chore: manifest update (INTERNET)

--- qwen_chat_openai/android/app/build.gradle.kts
+++ qwen_chat_openai/android/app/build.gradle.kts
@@ -8,7 +8,8 @@ plugins {
 android {
     namespace = "com.example.qwen_chat_openai"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    // Pin highest required NDK to avoid plugin mismatch warnings
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
